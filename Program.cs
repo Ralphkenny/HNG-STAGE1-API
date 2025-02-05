36cf8cf2d116ba6b659f5fa8a96fc25d3a98a9e6
Kennedy O <kennedyokpala12@gmail.com>
2025-02-05 12:46:32 +0000

    CORS

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ var app = builder.Build();
 
 app.UseResponseCompression();
 app.UseHttpsRedirection();
-app.UseCors(); // Apply CORS settings
+app.UseCors("AllowAll"); // Apply CORS settings
 app.UseAuthorization();
 app.MapControllers();
 
